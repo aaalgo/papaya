ada2b874d4ac370b075cea792297608c5069fdb3
Wei Dong <wdong@wdong.org>
2017-10-03 16:12:49 -0400

    update

---
+++ papaya.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import os
+import sys
+import shutil
+import subprocess
+from glob import glob
+from jinja2 import Environment, FileSystemLoader
+
+BASE = os.path.abspath(os.path.dirname(__file__))
+TMPL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                './templates')
+STATIC_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                './static')
+env = Environment(loader=FileSystemLoader(searchpath=TMPL_DIR))
+case_tmpl = env.get_template('papaya_case.html')
+index_tmpl = env.get_template('papaya_index.html')
+
+class Annotations:
+    def __init__ (self):
+        self.annos = []
+        pass
+
+    def add (self, box, hint=None):
+        self.annos.append({'box': box, 'hint': hint})
+        pass
+
+def Papaya (path, input_path, annotations=Annotations(), images = None, text = ''):
+    try:
+        os.makedirs(path)
+    except:
+        pass
+    try:
+        subprocess.check_call('rm -rf %s/dcm' % path, shell=True)
+    except:
+        pass
+        #try:
+        #    data = os.path.abspath(DATA_DIR)
+        #    os.symlink(data, os.path.join(path, 'data'))
+        #except:
+        #    pass
+    for f in ['papaya.css', 'papaya.js']:
+        shutil.copyfile(os.path.join(STATIC_DIR, f), os.path.join(path, f))
+        pass
+    pass
+    os.mkdir(os.path.join(path, 'dcm'))
+    subprocess.check_call('rm -rf %s/case.nii.gz' % (path, ), shell=True)
+    subprocess.check_call('%s/dcm2niix -z i -o %s -f case %s' % (BASE, path, input_path), shell=True)
+    boxes = []
+    centers = []
+    for anno in annotations.annos:
+        box = case.papaya_box(anno['box'])
+        boxes.append(box)
+        z1, y1, x1, z2, y2, x2 = box
+        hint = anno.get('hint', None)
+        center = ((z1+z2)/2, (y1+y2)/2, (x1+x2)/2,hint)
+        centers.append(center)
+    with open(os.path.join(path, 'index.html'), 'w') as f:
+        f.write(case_tmpl.render(boxes=boxes, centers=centers))
+        pass
+    pass
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', nargs=1, help='')
+    parser.add_argument('output', nargs=1, help='')
+    args = parser.parse_args()
+    #from adsb3 import Case
+    #case = Case('008464bb8521d09a42985dd8add3d0d2')
+    Papaya(args.output[0], args.input[0])
+    #boxes =[[38, 359, 393, 42, 367, 404], [63, 189, 138, 64, 201, 156], [64, 208, 82, 66, 218, 90], [126, 227, 343, 128, 237, 351], [138, 385, 180, 139, 391, 186]]
+    #papaya.next(case, boxes=boxes)
+    #papaya.flush()
